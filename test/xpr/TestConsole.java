1ad1716939c13eb5cace44d7abd9556bc7559c0d
pino1068 <giuseppe.dipierri@gmail.com>
2018-01-28 14:31:16 +0100

    proving posts

--- test/xpr/TestConsole.java
+++ test/xpr/TestConsole.java
@@ -5,6 +5,7 @@ import java.util.List;
 
 public class TestConsole implements Console {
 
+	private static final String SHELL = "> ";
 	private List<String> out = new ArrayList<>();
 	private String time = "( 0 seconds ago)";
 
@@ -16,7 +17,7 @@ public class TestConsole implements Console {
 	@Override
 	public void println(String line) {
 		String timeAgo = time+" ago";
-		out.add(line +" ("+timeAgo+")");
+		out.add(SHELL+line +" ("+timeAgo+")");
 	}
 
 	public List<String> lines() {
--- test/xpr/TwitterTest.java
+++ test/xpr/TwitterTest.java
@@ -21,16 +21,47 @@ public class TwitterTest {
 	
 	@Test
 	public void silence() throws Exception {
-		assertTrue(console.lines().isEmpty());
 		twitter.send("Alice");
+		
 		assertTrue(console.lines().isEmpty());
 	}
 	
 	@Test
 	public void alicePosts() throws Exception {
 		console.addTime("5 minutes");
+		
 		twitter.send("Alice -> I love the weather today");
+		
 		twitter.send("Alice");
 		assertEquals("> I love the weather today (5 minutes ago)",console.lines().get(0));
+	}
+	
+	@Test
+	public void bobPosts() throws Exception {
+		console.addTime("5 minutes");
+		
+		twitter.send("Bob -> Damn! We lost!");
+		twitter.send("Bob -> Good game though.");
+		
+		twitter.send("Bob");
+		assertEquals(2, console.lines().size());
+		assertEquals("> Damn! We lost! (5 minutes ago)",console.lines().get(0));
+		assertEquals("> Good game though. (5 minutes ago)",console.lines().get(1));
+	}
+	
+	@Test
+	public void aliceAndBobPost() throws Exception {
+		console.addTime("5 minutes");
+		
+		twitter.send("Alice -> I love the weather today");
+		twitter.send("Bob -> Damn! We lost!");
+		twitter.send("Bob -> Good game though.");
+		
+		twitter.send("Alice");
+		twitter.send("Bob");
+		assertEquals(3, console.lines().size());
+		assertEquals("> I love the weather today (5 minutes ago)",console.lines().get(0));
+		assertEquals("> Damn! We lost! (5 minutes ago)",console.lines().get(1));
+		assertEquals("> Good game though. (5 minutes ago)",console.lines().get(2));
 	}
 }
